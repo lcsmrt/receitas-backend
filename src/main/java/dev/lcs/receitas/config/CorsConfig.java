17f5b6f8b2fcf3b37e60de2605f413e075fbbe30
Lucas Martins <lucas.mrt.dev@gmail.com>
2025-12-16 23:00:22 -0300

    fix: usa filter explicito no cors para ver se config é aplicada corretamente

--- src/main/java/dev/lcs/receitas/config/CorsConfig.java
+++ src/main/java/dev/lcs/receitas/config/CorsConfig.java
@@ -3,8 +3,8 @@ package dev.lcs.receitas.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import java.util.List;
 
@@ -12,14 +12,18 @@ import java.util.List;
 public class CorsConfig {
 
     @Bean
-    public CorsConfigurationSource configurationSource() {
+    public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
+
         config.setAllowedOrigins(List.of("http://localhost:5173"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+        config.setAllowCredentials(true);
 
-        UrlBasedCorsConfigurationSource src = new UrlBasedCorsConfigurationSource();
+        UrlBasedCorsConfigurationSource src =
+                new UrlBasedCorsConfigurationSource();
         src.registerCorsConfiguration("/**", config);
-        return src;
+
+        return new CorsFilter(src);
     }
 }
